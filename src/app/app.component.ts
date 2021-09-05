eb637be82f12e601578ffa15ecc5b9660d52a230
benedsmith <besmith166@gmail.com>
2021-09-05 16:02:12 +0100

    getStock method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stock-screener';
+  apiBaseUrl = 'https://api.twelvedata.com';
+  apiKey = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('assets/apikey.txt',
+      { responseType: 'text'}).subscribe(data => { this.apiKey = data, this.getStock("AAPL");});
+  }
+
+  async getStock (stockSymbol: string) {
+    let reqUrl: string = this.apiBaseUrl + '/time_series?symbol=' + stockSymbol + '&interval=1h&apikey=' + this.apiKey;
+    this.http.get(reqUrl,
+      { responseType: 'json'}).subscribe(data => console.log(data))
+  };
+
+
 }
+
+
